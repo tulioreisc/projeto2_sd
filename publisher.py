dc8307966d5e98665c87b02274dc85df18bc64c5
LeoUtida <leoutida@hotmail.com>
2019-11-26 21:56:37 -0300

    MDS ACABA LOGO

--- publisher.py
+++ publisher.py
@@ -9,30 +9,31 @@ import random
 import sys
 import time
 from multiprocessing import Process
+import datetime
+import json
+import pickle
 
 # Dicionario global com as salas a serem monitoradas e seus respectivos codigos
-rooms = {"1":"LE1",
-	"2":"LE2",
-	"3":"LE3",
-	"4":"LE4",
-	"5":"LE5",
-	"6":"LE6",
-	"7":"PPGCC1",
-	"8":"PPGCC2",
-	"9":"PPGCC3",
-	"10":"PPGCC4",
-	"11":"Auditorio",
-	"12":"Almoxarifado",
-	"13":"Sala de Banco de dados",
-	"14":"Secretaria",
-	"15":"Sala de reuniao"}
+rooms = {"A":"LE1",
+		"B":"LE2",
+		"C":"LE3",
+		"D":"LE4",
+		"E":"LE5",
+		"F":"LE6",
+		"G":"PPGCC1",
+		"H":"PPGCC2",
+		"I":"PPGCC3",
+		"J":"PPGCC4"}
 
 # Esta funcao gera um valor aleatorio de temperatura para uma sala
 # passada como parametro. Com isso, monta uma mensagem do tipo
 # "Nome_da_sala : temperatura" eh gerada
 def set_message(topic):
-	message = str(rooms[topic]) + " : " + str(random.uniform(10.0, 30.0)) + "\n"
-	return message
+	val = random.uniform(10.0, 35.0)
+	# message = str(rooms[topic]) + " : " + str(val) + "\n"
+	json_data = {"sala":str(rooms[topic]),"topic":str(topic), "temp":str(val) ,"timestamp":str(datetime.datetime.now())}
+	json_obj = json.dumps(json_data)
+	return str(topic) + ":" + str(json_obj)
  
 # Funcao do publisher.
 # Cada publisher representa uma sala de aula do DC.
@@ -53,12 +54,14 @@ def pub(topic):
 	while True:
 		# Gera uma mensagem com a funcao mostrada acima para o topico especifico
 		message = set_message(topic)
+		print(message)
 
 		#Mostra as informacoes geradas
-		print "%d#%s#\n%s" % (room_update_id, topic, message)
+		print "%d#%s#%s#\n" % (room_update_id, topic, message)
 		
 		# Envia os dados acima e de suas empresas para os subscribers interessados no topico
-		socket.send("%s#%d#\n%s" % (topic, room_update_id, message))
+		socket.send(message)
+		print("############ENVIOU###############")
 		
 		# Atualiza o valor da atualizacao
 		room_update_id = room_update_id + 1
@@ -69,7 +72,9 @@ def main():
 	# Cria um processo para tratar de sala de aula
 	# Os processos irao executar a funcao pub descrita acima
 	for r in rooms:
-		Process(target=pub, args=(r)).start()
+		print("Starting process for room:", r)
+		print(type(r))
+		Process(target=pub, args=(r,)).start()
 
 if __name__ == '__main__':
 	main()
--- subscriber.py
+++ subscriber.py
@@ -8,7 +8,11 @@ import sys
 import zmq
 from multiprocessing import Process
 import os
+import datetime
 import time
+from statistics import mean 
+import json
+import pickle
 
 # Funcao do subscriber. 
 # Cada subscriber eh um cliente que gostaria de monitorar uma temperatura
@@ -21,7 +25,7 @@ def sub(topicfilter):
 	socket = context.socket(zmq.SUB)
 	socket.connect ("tcp://localhost:%s" % port)
 	socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
-
+	
 	# Cria o o log dos valores lidos da acao. Para cada cliente sera gerado um 
 	# arquivo de log com os valores de temperatura de sala que ele leu acompanhado dos 
 	# IDs destes valores.
@@ -33,22 +37,48 @@ def sub(topicfilter):
 	log_file = open('sub_log_' + str(log_id) + '.txt', 'a')
 
 
+	cont = 0
+	meanTemp = []
 	while True:
 		# Recebe os valores do publisher escolhido
-	    string = socket.recv()
+		msg = socket.recv()
+		msg = msg.split(':',1)[1]
+		print(msg)
+		json_data = json.loads(msg)
 
-	    # Obtem cada informacao, realizando um split na string recebida
-	    topic, update_id, messagedata = string.split("#")
-
-	    # Mostra as informacoes na tela e as escreve no log
-	    print("Sala: " + str(topic))
-	    file.write(log_file, "Sala: " + str(topic) + "\n")
-	    print("Update ID: " + str(update_id))
-	    file.write(log_file, "Update ID: " + str(update_id) + "\n")
-	    print(messagedata)
-	    file.write(log_file, messagedata + "\n")
-	    time.sleep(0.1)
-	    os.system('clear')
+		#  Obtem cada informacao, realizando um split na json_data recebida
+		# topic, update_id, timeStamp, val, messagedata = json_data.split("#")
+		topic = json_data["topic"]
+		timeStamp = json_data["timestamp"]	
+		val = json_data["temp"]
+		sala = json_data["sala"]
+		if(cont<=10):
+			print("Entrou no cont<10")
+			meanTemp.append(float(val))
+			# Mostra as informacoes na tela e as escreve no log
+			print("Topic: " + str(topic))
+			print("Sala: " + str(sala))
+			print("Update ID: " + str(timeStamp))
+			print("Temp: " + str(val))
+			time.sleep(0.1)
+			os.system('clear')
+			cont = cont + 1
+		else:
+			print("Entrou no outro")
+			meanTemp.append(float(val))
+			meanTemp = meanTemp[1:]
+			# Mostra as informacoes na tela e as escreve no log
+			print("Topic: " + str(topic))
+			file.write(log_file, "Topic: " + str(topic) + "\n")
+			print("Sala: " + str(sala))
+			file.write(log_file, "Sala: " + str(sala) + "\n")
+			print("Update ID: " + str(timeStamp))
+			file.write(log_file, "Update ID: " + str(timeStamp) + "\n")
+			print("\nMedia de temp: " + str(mean(meanTemp)))
+			file.write(log_file, str(mean(meanTemp)) + "\n")
+			time.sleep(0.1)
+			os.system('clear')
+			cont = cont + 1
 
 	log_file.close()
 
@@ -56,21 +86,16 @@ def main():
 
 # Menu inicial mostrando as salas e seus respectivos IDs
 	print("Opcao    Sala")
-	print('1		LE1')
-	print('2		LE2')
-	print('3		LE3')
-	print('4		LE4')
-	print('5		LE5')
-	print('6		LE6')
-	print('7		PPGCC1')
-	print('8		PPGCC2')
-	print('9		PPGCC3')
-	print('10		PPGCC4')
-	print('11		Auditorio')
-	print('12		Almoxarifado')
-	print('13		Sala de Banco de dados')
-	print('14		Secretaria')
-	print('15		Sala de reuniao')
+	print('A: LE1')
+	print('B: LE2')
+	print('C: LE3')
+	print('D: LE4')
+	print('E: LE5')
+	print('F: LE6')
+	print('G: PPGCC1')
+	print('H: PPGCC2')
+	print('I: PPGCC3')
+	print('J: PPGCC4')
 
 	# Selecao de topico
 	topicfilter = raw_input("Escolha a sala de aula que deseja monitorar: ")
